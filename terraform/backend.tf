42402422c2c4b7c7c402485aebbb787cc8f18248
Totoratsu <nixon.lizcano@pyxisportal.co>
2022-12-26 17:47:19 -0500

    docs1

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-1672083986-bucket"
-    key            = "terraform/terraform.tfstate"
-    dynamodb_table = "terraform_tf_state_1672083986"
-    region         = "us-east-1"
-  }
-}
